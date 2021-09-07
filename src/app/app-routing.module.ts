356a8c326170a11ea28203257014d43da861a8e5
Mashimot <linkhashimoto@hotmail.com>
2021-09-07 19:53:40 -0300

    Defining default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: 'json-to-form',
     loadChildren: () => import ("./json-to-form/json-to-form.module").then(m => m.JsonToFormModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import ("./json-to-form/json-to-form.module").then(m => m.JsonToFormModule)
   }
 ];
 
