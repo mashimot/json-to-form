56c12c5806c145fad1c84009d2439aade8f6ae6a
Mashimot <linkhashimoto@hotmail.com>
2021-09-07 19:47:00 -0300

    Removing test-form  module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { JsonToFormModule } from './json-to-form/json-to-form.module';
 import { RouterModule } from '@angular/router';
-import { TestFormComponent } from './test-form/test-form.component';
 
 @NgModule({
   declarations: [
