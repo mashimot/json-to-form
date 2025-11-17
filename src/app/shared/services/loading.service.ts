c169cf4e13cf882a3cdd99ab3102e3320bdca98a
Daniel <linkhashimoto@hotmail.com>
2025-11-17 00:13:29 -0300

    chore: switching to bootswatch darkly theme

--- src/app/shared/services/loading.service.ts
+++ src/app/shared/services/loading.service.ts
@@ -1,16 +1,19 @@
-import { BehaviorSubject, Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoadingService {
   private isLoadingSubject = new BehaviorSubject<boolean>(false);
-  public isLoadingAction$ = this.isLoadingSubject.asObservable();
 
   constructor() {}
 
-  public isLoading(state: boolean) {
+  public isLoading(state: boolean): void {
     this.isLoadingSubject.next(state);
+  }
+
+  public getLoading(): Observable<boolean> {
+    return this.isLoadingSubject.asObservable();
   }
 }
